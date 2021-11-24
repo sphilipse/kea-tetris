666cdb6773f8ccdb08111ecbc400886cae1ea178
Sander Philipse <sander.philipse@elastic.co>
2021-11-24 12:45:32 +0100

    Initialize project using Create React App

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { Provider } from 'kea'
+import { initKea } from './initKea'
+
+initKea()
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
